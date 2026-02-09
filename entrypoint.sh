7d9d9c31e7b56daf42e3a2c367b2550fee42fbcf
Javohir <qobiljonsharipov33@gmail.com>
2026-02-09 18:52:47 +0500

    Edited some settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic --no-input
 
 python manage.py makeadmin 
 
-gunicorn config.wsgi:application -w 4 -b 0.0.0.0:8000
+gunicorn config.wsgi:application -w 2 -b 0.0.0.0:8000
