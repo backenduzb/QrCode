f2876a32916c11b0f7b884f9f06d5ea1ab5611e7
Javohir <qobiljonsharipov33@gmail.com>
2026-02-09 14:51:01 +0500

    Edited settings for railway settings

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('', include('docs.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
