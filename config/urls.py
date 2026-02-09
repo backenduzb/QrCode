b2ae7f6692f0368f95d79acbe29501494eeda184
Javohir <qobiljonsharipov33@gmail.com>
2026-02-09 19:18:29 +0500

    Edited some settings

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
 ]
 
 urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
