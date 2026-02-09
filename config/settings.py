513dd5048c57b640063519813ef6c6ec6f391b45
Javohir <qobiljonsharipov33@gmail.com>
2026-02-09 14:44:04 +0500

    Edited settings for railway settings

--- config/settings.py
+++ config/settings.py
@@ -106,8 +106,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/media"
+
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
